051be76da331f07b5cc75b9d5ed2320055f0ad81
Raj Nandan Sharma <raj@cashfree.com>
2024-01-21 13:46:01 +0530

    Monitors can now have categories with every category having their dedicated page

---
+++ src/routes/category-[category]/+page.server.js
@@ -0,0 +1,33 @@
+// @ts-nocheck
+import { GetIncidents, Mapper } from "../../../scripts/github.js";
+import { FetchData } from "$lib/server/page";
+import { env } from "$env/dynamic/public";
+import fs from "fs-extra";
+
+export async function load({ params, route, url, parent }) {
+	let monitors = JSON.parse(fs.readFileSync(env.PUBLIC_KENER_FOLDER + '/monitors.json', "utf8"));
+    const parentData = await parent();
+    const siteData = parentData.site;
+    const github = siteData.github;
+    const monitorsActive = [];
+	for (let i = 0; i < monitors.length; i++) {
+		if (monitors[i].hidden !== undefined && monitors[i].hidden === true) {
+			continue;
+        }
+		//only return monitors that have category as home or category is not present
+		if (monitors[i].category === undefined || monitors[i].category !== params.category) {
+			continue;
+		}
+       	const gitHubActiveIssues = await GetIncidents(monitors[i].tag, github, "open");
+		delete monitors[i].api;
+		delete monitors[i].defaultStatus;
+		let data = await FetchData(monitors[i], parentData.localTz);
+		monitors[i].pageData = data;
+		monitors[i].activeIncidents = await Promise.all(gitHubActiveIssues.map(Mapper, { github }));
+		monitorsActive.push(monitors[i]);
+	}
+
+    return {
+        monitors: monitorsActive,
+    };
+}
