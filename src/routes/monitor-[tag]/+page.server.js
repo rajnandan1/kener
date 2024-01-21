42e2024d71c62a9c955f60431f944a9c112cd33e
Raj Nandan Sharma <raj@cashfree.com>
2024-01-21 13:49:26 +0530

    Monitors now have their dedicated page

---
+++ src/routes/monitor-[tag]/+page.server.js
@@ -0,0 +1,31 @@
+// @ts-nocheck
+import { GetIncidents, Mapper } from "../../../scripts/github.js";
+import { FetchData } from "$lib/server/page";
+import { env } from "$env/dynamic/public";
+import fs from "fs-extra";
+
+export async function load({ params, route, url, parent }) {
+	let monitors = JSON.parse(fs.readFileSync(env.PUBLIC_KENER_FOLDER + '/monitors.json', "utf8"));
+    const parentData = await parent();
+    const siteData = parentData.site;
+    const github = siteData.github;
+    const monitorsActive = [];
+	for (let i = 0; i < monitors.length; i++) {
+		 
+		//only return monitors that have category as home or category is not present
+		if (monitors[i].tag !== params.tag) {
+			continue;
+		}
+       	const gitHubActiveIssues = await GetIncidents(monitors[i].tag, github, "open");
+		delete monitors[i].api;
+		delete monitors[i].defaultStatus;
+		let data = await FetchData(monitors[i], parentData.localTz);
+		monitors[i].pageData = data;
+		monitors[i].activeIncidents = await Promise.all(gitHubActiveIssues.map(Mapper, { github }));
+		monitorsActive.push(monitors[i]);
+	}
+
+    return {
+        monitors: monitorsActive,
+    };
+}
