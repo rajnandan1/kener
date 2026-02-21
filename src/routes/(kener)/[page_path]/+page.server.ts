1c6bed129711e8c684195c77a4714c8266288690
Raj Nandan Sharma <rajnandan1@gmail.com>
2026-02-21 20:11:44 +0530

    changes

--- src/routes/(kener)/[page_path]/+page.server.ts
+++ src/routes/(kener)/[page_path]/+page.server.ts
@@ -1,10 +1,10 @@
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import { GetPageDashboardData } from "$lib/server/controllers/dashboardController.js";
-import { env } from "$env/dynamic/private";
 
-export const load: PageServerLoad = async ({ params }) => {
-  const dashboardData = await GetPageDashboardData(params.page_path);
+export const load: PageServerLoad = async ({ params, parent }) => {
+  const layoutData = await parent();
+  const dashboardData = await GetPageDashboardData(params.page_path, layoutData);
   if (!dashboardData) {
     throw error(404, "Page Not Found");
   }
