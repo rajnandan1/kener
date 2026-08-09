04dcf0467e24670369e5036b332d14972dbd1cac
AA <alberta@localhost.localdomain>
2026-08-09 23:23:18 +0200

    Update: use language key

--- src/lib/components/IncidentItem.svelte
+++ src/lib/components/IncidentItem.svelte
@@ -44,7 +44,7 @@
   <Item.Content class="min-w-0 flex-1">
     <div class="flex flex-col items-start justify-start gap-0.5">
       <span class="text-muted-foreground text-xs font-medium uppercase">
-        INCIDENT
+        {$t("incident")}
       </span>
       <span class="text-xs font-medium text-{incident.state.toLowerCase()}">{$t(incident.state)}</span>
       <Item.Title class="min-w-0 text-base wrap-break-word break-all">
--- src/lib/components/MaintenanceItem.svelte
+++ src/lib/components/MaintenanceItem.svelte
@@ -36,7 +36,7 @@
   <Item.Content class="min-w-0 flex-1">
     <div class="flex flex-col items-start justify-start gap-0.5">
       <span class="text-muted-foreground text-xs font-medium uppercase">
-        MAINTENANCE
+        {$t("maintenance")}
       </span>
       <span class="text-xs font-medium text-{maintenance.status.toLowerCase()}">{$t(maintenance.status)}</span>
       <Item.Title class="min-w-0 text-base wrap-break-word break-all">
