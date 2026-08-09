144a10bd95f95e241341398ee06354c68ff80a8d
AA <alberta@localhost.localdomain>
2026-08-09 20:10:13 +0200

    Try: add flag for maintenance inline event

--- src/lib/components/MaintenanceItem.svelte
+++ src/lib/components/MaintenanceItem.svelte
@@ -35,6 +35,9 @@
 <Item.Root class="items-start p-0 {className} sm:items-center">
   <Item.Content class="min-w-0 flex-1">
     <div class="flex flex-col items-start justify-start gap-0.5">
+      <span class="text-muted-foreground text-xs font-medium uppercase">
+        MAINTENANCE
+      </span>
       <span class="text-xs font-medium text-{maintenance.status.toLowerCase()}">{$t(maintenance.status)}</span>
       <Item.Title class="min-w-0 text-base wrap-break-word break-all">
         <a {target} class="hover:underline" href={clientResolver(resolve, `/maintenances/${maintenance.id}`)}
